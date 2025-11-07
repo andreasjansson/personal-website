76c5d624e571d55238f699bf2e13d46d6748bc92
andreasjansson <andreas@replicate.ai>
2025-11-07 16:36:51 +0100

    Further reduce gyroid influence by increasing alpha offset

--- blobby-gyroid/optimize.py
+++ blobby-gyroid/optimize.py
@@ -62,7 +62,7 @@ class BlobbyGyroid(nn.Module):
         # --- Gyroid core
         self.omega = nn.Parameter(torch.tensor([1.6, 1.7, 1.8]))  # per-axis freq
         self.phi = nn.Parameter(torch.zeros(3))
-        self.alpha = nn.Parameter(torch.tensor(2.0))  # increase to reduce gyroid influence (G-alpha)
+        self.alpha = nn.Parameter(torch.tensor(3.0))  # large alpha to make G≈0 on average
 
         # --- Metaballs
         self.N = N
